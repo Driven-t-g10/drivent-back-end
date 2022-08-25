71e2ee70d9980f8c2373e00845a6c4da1142e55c
Paulo Uilian <65803142+WillianBL99@users.noreply.github.com>
2022-08-25 14:28:23 -0300

    Update ticket-router.ts

--- src/routers/ticket-router.ts
+++ src/routers/ticket-router.ts
@@ -4,7 +4,7 @@ import { getTickets } from '@/controllers';
 import { authenticateToken } from '@/middlewares';
 
 const ticketsRouter = Router();
-ticketsRouter.all('/*', authenticateToken);
+ticketsRouter.all('*', authenticateToken);
 ticketsRouter.get('/', getTickets);
 
 export { ticketsRouter };
