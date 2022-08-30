f01c9fce34e08b51e9a7eae08c723845c777ed4e
Maria Luiza <maria.lmm.975@gmail.com>
2022-08-30 15:01:35 -0300

    fix: change getHotel response

--- src/repositories/hotel-repository/index.ts
+++ src/repositories/hotel-repository/index.ts
@@ -1,7 +1,15 @@
 import { prisma } from '@/config';
 
 async function getHotels() {
-  return prisma.hotel.findMany();
+  const hotels = await prisma.$queryRaw`SELECT h.id, h.name, 
+  SUM(r.beds) AS spaces, 
+  COUNT(ur.id) AS occupied 
+  FROM "Hotel" h
+  JOIN "Room" r ON r."hotelId" = h.id
+  LEFT JOIN "UserRoom" ur ON ur."roomId" = r.id
+  GROUP BY h.id`;
+
+  return hotels;
 }
 
 const hotelRepository = { getHotels };
