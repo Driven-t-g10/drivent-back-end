87ad2d47c7ce195768cff4f225abe98caad4b10c
Brenoqn2 <brenoqn2@gmail.com>
2022-09-08 16:58:48 -0300

    require authenticateToken to get activities info

--- src/routers/activities-router.ts
+++ src/routers/activities-router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import { getDates, getPlaces, getActivitiesByPlaceAndDate } from '@/controllers/activities-controller';
-// import { authenticateToken } from '@/middlewares';
+import { authenticateToken } from '@/middlewares';
+
 const activitiesRouter = Router();
 
-// activitiesRouter.all('*', authenticateToken);
+activitiesRouter.all('*', authenticateToken);
 activitiesRouter.get('/dates', getDates);
 activitiesRouter.get('/places', getPlaces);
 activitiesRouter.get('', getActivitiesByPlaceAndDate);
